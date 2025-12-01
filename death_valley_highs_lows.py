57d63dcb3731351d54b6e9a84997342038668c92
oleg <o1egov@mail.ru>
2025-12-01 17:45:42 +0700

    добавим последние изменение

--- death_valley_highs_lows.py
+++ death_valley_highs_lows.py
@@ -1,8 +1,8 @@
 import csv
-from matplotlib import pyplot as plt
 from datetime import datetime
+from matplotlib import pyplot as plt
 
-filename = 'data/sitka_weather_2018_simple.csv'
+filename = 'data/death_valley_2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -11,11 +11,15 @@ with open(filename) as f:
     dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
-        high = int(row[5])
-        low = int(row[6])
-        dates.append(current_date)
-        highs.append(high)
-        lows.append(low)
+        try:
+            high = int(row[4])
+            low = int(row[5])
+        except ValueError:
+            print(f'Missing data for {current_date}')
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 # нанесение данных на диаграмму
 plt.style.use('classic')
@@ -25,7 +29,7 @@ ax.plot(dates, lows, c='blue', alpha=0.5)
 plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # форматирование диаграммы
-plt.title('Daile high and low temperatures - 2018', fontsize=24)
+plt.title('Daile high and low temperatures - 2018\nDeath valley', fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel('Temperature (F)', fontsize=16)
