00e7faad53a3488269e864070cd18b8f21f6ecae
oleg <o1egov@mail.ru>
2025-12-14 18:23:27 +0700

    работа со словарем ответа

--- python_repos.py
+++ python_repos.py
@@ -8,6 +8,26 @@ print(f"Status code: {r.status_code}")
 
 # сохранение API в переменной.
 response_dict = r.json()
+print(f'Total repositories: {response_dict['total_count']}')
+
+# анализ информации о репозиториях
+repo_dicts = response_dict['items']
+print(f'Repositories returned: {len(repo_dicts)}')
+
+# анализ первого репозитория
+repo_dict = repo_dicts[0]
+
+print('\nSelected information about first repository:')
+print(f'Name: {repo_dict['name']}')
+print(f'Owner: {repo_dict['owner']['login']}')
+print(f'Stars: {repo_dict['stargazers_count']}')
+print(f'Repository: {repo_dict['html_url']}')
+print(f'Created: {repo_dict['created_at']}')
+print(f'Updated: {repo_dict['updated_at']}')
+print(f'Description: {repo_dict['description']}')
+print(f'\nKeys: {len(repo_dict)}')
+for key in sorted(repo_dict.keys()):
+    print(key)
 
 # обработка результатов
 print(response_dict.keys())
