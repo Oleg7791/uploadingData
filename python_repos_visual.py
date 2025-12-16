88439e603fddf8e67b556014af119de0ec962346
oleg <o1egov@mail.ru>
2025-12-16 19:54:50 +0700

    доработка диаграмм Plotly

--- python_repos_visual.py
+++ python_repos_visual.py
@@ -6,7 +6,7 @@ from plotly import offline
 # создание вызова API и сохранение ответа
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 headers = {'Accept': 'application/vnd.github.v3 +json'}
-r = requests.get(url,headers=headers)
+r = requests.get(url, headers=headers)
 print(f"Status code: {r.status_code}")
 
 # сохранение API в переменной.
@@ -18,7 +18,6 @@ repo_dicts = response_dict['items']
 print(f'Repositories returned: {len(repo_dicts)}')
 repo_names, stars = [], []
 
-
 print('\nSelected information about each repository:')
 for repo_dict in repo_dicts:
     repo_names.append(repo_dict['name'])
@@ -27,13 +26,26 @@ for repo_dict in repo_dicts:
 # построение визуализации
 data = [{
     'type': 'bar',
-    'x':repo_names,
-    'y': stars
+    'x': repo_names,
+    'y': stars,
+    'marker': {
+        'color': 'rgb(60, 100, 150)',
+        'line': {'width': 2.5, 'color': 'rgb(25,25,250)'}
+    },
+    'opacity': 0.6
 }]
 my_layout = {
     'title': 'Most-Starred Python Projects on GitHub',
-    'xaxis': {'title':'Repository'},
-    'yaxis': {'title': 'Stars'},
+    'font': {'family':'Brush Script MT','size': 28,'color':'green',},
+
+    'xaxis': {'title': {
+        'text':'Repository',
+        'font':{'family':'Brush Script MT','size': 24,'color':'red'}},
+        'tickfont': {'family':'Times New Roman','size': 10}},
+    'yaxis': {'title': {
+        'text':'Stars',
+        'font':{'family':'Times New Roman','size': 24,'color':'black'}},
+        'tickfont': {'size': 16,'color':'black'}},
 }
-fig = {'data':data, 'layout': my_layout}
+fig = {'data': data, 'layout': my_layout}
 offline.plot(fig, filename='python_repos.html')
